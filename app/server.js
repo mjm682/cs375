ebf1fda9fb1233dcaa8652405d35754ce0507e0b
Mike Martin <mjm682@drexel.edu>
2021-02-27 21:40:03 -0500

    began spotify integration
    Renamed home.html to index.html so that the page can be
    visited from localhost:3000. Added search by hitting enter key.
    Began adding logic for Spotify API call. There is an error in the
    server.js where the artist name isnt being parsed or something.
    Added a package.json and env.json file with a spotify auth key.

--- app/server.js
+++ app/server.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app = express();
 
+let axios = require("axios");
+let env = require("./env.json");
+let token = env["token"];
+
 const port = 3000;
 const hostname = "localhost";
 
 app.use(express.json());
 app.use(express.static("public_html"));
+
+let config = {
+    headers: { Authorization: `Bearer ${token}` }
+};
+
+app.get("/search", function(req, res) {
+    let artist = req.query.artist;
+    console.log(artist);
+    axios.get(`https://api.spotify.com/v1/search?q=${artist}&type=artist`, config).then(function (response) {
+        console.log(`Searching for artist ${artist}`);
+        res.status(200);
+    });
+});
+app.get("/result", function(req, res) {
+    console.log("result");
+    return res.status(200).send("");
+});
 
 app.listen(port, hostname, () => {
     console.log(`Listening at: http://${hostname}:${port}`);
