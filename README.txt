3689fae7d670372158294337c617c2bbe7484521
Mike Martin <mjm682@drexel.edu>
2021-03-19 20:25:27 -0400

    searchify - final submission

---
+++ README.txt
@@ -0,0 +1,24 @@
+Team Name: S3M
+Team Members: Chance Shipley, Devan Suber, & Michael Martin
+App: Searchify 
+Class: CS375
+
+--------------------- To use Searchify ---------------------
+
+$ cd searchify
+$ npm install
+$ cd app
+$ node server.js
+
+* The app will be running from http://localhost:3000/
+
+You may signin to the following dummy Spotify account when prompted:
+
+    Email : S3M.CS375@gmail.com
+    Password : Drexel2020Winter!
+
+If you need access to the Gmail (for some reason) use the same credentials.
+You once you authorize the account, you may begin using the app.
+
+*NOTE* depending on your browser settings, you may be prompted to authorize 
+your account on every search.
--- app/server.js
+++ app/server.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const app = express();
-var request = require('request'); // "Request" library
+let request = require('request');
 
-var favicon = require('serve-favicon');
+let favicon = require('serve-favicon');
 app.use(favicon(__dirname + '/favicon/favicon-32x32.png'));
 
 let axios = require("axios");
-var querystring = require('querystring');
+let querystring = require('querystring');
 
-var client_id = 'b0e5f1805df1454daa59e96c976dc66d'; // Your client id
-var client_secret = '5c027ea2b9bd4e3095f718ddce2ec43d'; // Your secret
-var redirect_uri = 'http://localhost:3000/search.html'; // Your redirect uri
+let client_id = 'b0e5f1805df1454daa59e96c976dc66d';
+let client_secret = '5c027ea2b9bd4e3095f718ddce2ec43d'; 
+let redirect_uri = 'http://localhost:3000/search.html'; 
 
-var searchedArtist = '';
-var searchedFollowers = '';
-var searchedGenre = '';
-var searchedURI = '';
+let searchedArtist = '';
+let searchedFollowers = '';
+let searchedGenre = '';
+let searchedURI = '';
 let searchedURL = '';
 
 const port = 3000;
@@ -34,10 +34,10 @@ app.get('/login', function(req, res) {
   });
 
 app.get('/search', function(req, res) { 
-  var code = req.query.code || null;
+  let code = req.query.code || null;
   let artist = req.query.artist;
-  console.log("code: " + code);
-  var authOptions = {
+  console.log("Auth Code: " + code);
+  let authOptions = {
     url: 'https://accounts.spotify.com/api/token',
     form: {
       code: code,
@@ -54,50 +54,39 @@ app.get('/search', function(req, res) {
     console.log(response.statusCode);
     if (!error) {
 
-      var access_token = body.access_token,
-          refresh_token = body.refresh_token;
-        console.log("access token = " + access_token);
-        console.log("refresh token = " + refresh_token);
+      let access_token = body.access_token;
+      console.log("Acces Token: " + access_token);
 
-      var options = {
+      let options = {
         url: `https://api.spotify.com/v1/search?q=${artist}&type=artist`,
         headers: { 'Authorization': 'Bearer ' + access_token },
         json: true
       };
 
-      console.log("Artist:" + artist);
-      console.log("Token = " + access_token);
-      //console.log(`Searching for artist ${artist}`);
-
       axios.get(`https://api.spotify.com/v1/search?q=${artist}&type=artist`, options).then(function (response){
-        console.log(`Searching for artist ${artist}`);
-        console.log(response.data.artists.items);
+        console.log(`Searching for ${artist}`);
         searchedArtist = response.data.artists.items[0].name;
         searchedFollowers = response.data.artists.items[0].followers.total;
         searchedGenre = response.data.artists.items[0].genres[0];
         searchedURI = response.data.artists.items[0].id;
-        searchedURL = response.data.artists.items[0].images[0].url;
 		    searchedPopularity = response.data.artists.items[0].popularity;
+
+        if (response.data.artists.items[0].images[0] === undefined){
+          searchedURL = 'default.jpg';
+        }
+        else{
+          searchedURL = response.data.artists.items[0].images[0].url;
+        }
 
         if (searchedGenre === undefined){
           searchedGenre = "Unknown";
         }
-        
-        //let newJSON = {"name" : artistName, "followers": followers, "genre": genre, "imageURL": imageURL, "uri": uri};
-        //console.log(newJSON);
-        //res.status(200).json(newJSON);
+
         let relatedURL = 'https://api.spotify.com/v1/artists/' + searchedURI + '/related-artists';
-        //let relatedArtists = await axios.get(relatedURL);
-        
-          console.log(access_token);
-          console.log(searchedArtist);
+
         return axios.get(relatedURL, options);
-        console.log(response);
-        console.log(newJSON);
         res.status(200).json(newJSON);
   }).then(function (response){
-      console.log(response.data);
-      // let artistName = response.data.artists[0].name;
 
       let newJSON = {}
       let artists = [];
@@ -128,13 +117,6 @@ app.get('/search', function(req, res) {
 
             let URI = response.data.artists[i].id;
             
-            console.log(imageURL);
-            console.log(artistName);
-            console.log(followers);
-            console.log(genre);
-            console.log(URI);
-            console.log(popularity);
-            
             searchedArtistInfo = {"name" : searchedArtist, "followers": searchedFollowers, "genre": searchedGenre, "imageURL": searchedURL, "uri": searchedURI, "popularity": popularity};
             artistInfo = {"name" : artistName, "followers": followers, "genre": genre, "imageURL": imageURL, "uri": URI, "popularity": popularity}
             artists[0] = searchedArtistInfo;
@@ -158,7 +140,6 @@ app.get('/search', function(req, res) {
     }
   });
 });
-
 
 app.listen(port, hostname, () => {
     console.log(`Listening at: http://${hostname}:${port}`);
